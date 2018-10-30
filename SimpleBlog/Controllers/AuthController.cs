2be86c4f32c64fcb318f550d4774e96fece6dff5
Greg Davis <gdavis@davtechsolutions.com>
2018-10-29 20:38:36 -0400

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,29 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+
+
+            //SEND DATA TO THE VIEW
+            //return View(new AuthLogin
+            //{
+            //    Test = "Test Data to view."  //Test property is defined in ViewModel AuthLogin.
+            //});
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            //Validating input and custom error msg.
+            if (form.Username != "Rainbow Dash")
+            {
+                ModelState.AddModelError("Username", "Username or password is uncool!");
+                return View(form);
+            }
+
+            return Content("The form is valid!");
         }
     }
 }
